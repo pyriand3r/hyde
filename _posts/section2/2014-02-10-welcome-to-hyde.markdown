ec3239e95c6d3c311ee334a3afd6381e9bd90bdf
Gabriel Jülke <pyriand3r@gmail.com>
2014-02-13 12:57:15 +0100

    gh-page fist commit

---
+++ _posts/section2/2014-02-10-welcome-to-hyde.markdown
@@ -0,0 +1,14 @@
+---
+layout: section2
+title:  "Welcome to Hyde!!"
+date:   2014-02-10 22:57:41
+categories: hyde update
+---
+
+So here is the dynamic frontend **Hyde** for your Jekyll Blog! Noticed the color changing effect in the background and title? And the transition effect when changing the content? That's **Hyde** allowing you to use transition effects etc. for your *dynamic* Jekyll Site.
+
+Hyde makes use of [AngularJS](http://angularjs.org) to inject the content of your blog into the site. To show you how it works, Hyde comes preconfigured with a nice dark theme and some effects. Use it as it is, change the CSS or create your own Style. To add effects and manipulate the DOM you can use [AngularJS](http://angularjs.org) or [jQuery](http://jquery.com)). Look at the **Hyde** Sourcecode and read the documentations for this two frameworks if you want to add custom animations. Also there are some informations in the Hyde-repository on [Github](https://github.com/pyriand3r/hyde).
+
+In addition there will be some Howtos on my blog [scriptaholic](http://pyriand3r.no-ip.org) soon.
+
+Have fun with jekyll and hyde!
---
+++ js/app.js
@@ -0,0 +1,72 @@
+/**
+* Main App File
+*/
+'use strict';
+
+angular.module('hyde', ['ngRoute', 'ngAnimate'])
+	/**
+	 * Sets main routing of app to provide dynamic loading of site content.
+	 */
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider
+			.when('/', {
+				// Link to your home.html. 
+				// If you want to use hyde as a github-page change this link to '/repository/home.html'
+				templateUrl: '/hyde/home.html'	
+			})
+			.when('/:link*', {
+				templateUrl: function(params) {
+					var link = params.link;
+					if (link.substr(link.length - 4, link.length) != 'html') {
+						link += '/index.html';
+					}
+					return link;
+				}
+			});
+	}])
+	/**
+	 * Override a-tag to prevent direct loading of partial. Links starting with 'http' and
+	 * 'www' are let through to allow linking to third-party website
+	 */
+	.directive('a', ['$location', function($location) {
+		return {
+			restrict: 'E',
+			link: function(scope, elem, attrs) {
+				elem.on('click', function(e) {
+					if (attrs.href.substr(0,4) == 'http' || attrs.href.substr(0,3) == 'www') {
+						return;
+					} else {
+						e.preventDefault();
+						$location.path(attrs.href);
+						scope.$apply();
+					} 
+				})
+			}
+		}
+	}])
+	/**
+	 * Directive for adding dynamic content. If you add a attribute to the additive-tag in your template,
+	 * you can use angular or jquery inside the link-function to manipulate the DOM to your needs. The
+	 * allows the change of the background-color for specific sites. As for now you have to set color for
+	 * every page to change color back to original when leaving colored page.
+	 */
+	.directive('additive', [function() {
+		return {
+			restrict: 'E',
+			scope: {
+				bgColor: '@',
+				color: '@'
+			},
+			link: function(scope, element, attrs) {
+				$('body').animate({
+					backgroundColor: scope.bgColor,
+				 	borderTopColor: scope.color
+				}, 1000);
+
+				$('#header > .title > a').animate({
+				 	'color': scope.color
+				});
+			}
+		}
+	}])
+;
